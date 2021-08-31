c2bbe41899064d667aa7583f9e7c3313d01f79b8
zyura10 <89729859+zyura10@users.noreply.github.com>
2021-08-31 09:49:52 +0700

    Update promote.js

--- plugins/promote.js
+++ plugins/promote.js
@@ -8,9 +8,9 @@ let handler = async (m, { conn, text }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
     else who = m.chat
     if (!who) throw `tag orangnya!`
-    if (global.prems.includes(who.split`@`[0])) throw 'dia udah premium!'
-    global.prems.push(`${who.split`@`[0]}`)
-    conn.reply(m.chat, `@${who.split`@`[0]} sekarang premium!`, m, {
+    if (global.admin.includes(who.split`@`[0])) throw 'dia udah admin!'
+    global.admin.push(`${who.split`@`[0]}`)
+    conn.reply(m.chat, `@${who.split`@`[0]} sekarang admin!`, m, {
         contextInfo: {
             mentionedJid: [who]
         }
