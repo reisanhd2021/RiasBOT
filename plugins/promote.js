cf7dfe548655675b769f68e6be532d2c9bedc28d
zyura10 <89729859+zyura10@users.noreply.github.com>
2021-09-04 20:16:02 +0700

    Update promote.js

--- plugins/promote.js
+++ plugins/promote.js
@@ -2,21 +2,6 @@ let handler = async (m, { conn, args }) => {
   let users = m.mentionedJid
   conn.groupMakeAdmin(m.chat, users)
 }
-let handler = async (m, { conn, text }) => {
-
-    let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
-    else who = m.chat
-    if (!who) throw `tag orangnya!`
-    if (global.admin.includes(who.split`@`[0])) throw 'dia udah admin!'
-    global.admin.push(`${who.split`@`[0]}`)
-    conn.reply(m.chat, `@${who.split`@`[0]} sekarang admin!`, m, {
-        contextInfo: {
-            mentionedJid: [who]
-        }
-    })
-
-}
 handler.help = ['promote','pm'].map(v => v + ' @user')
 handler.tags = ['group']
 handler.command = /^(promote|pm)$/i
